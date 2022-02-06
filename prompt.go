46df84a72bd583590af67709a84d48f0721d12de
Atharv Gupte <74566464+Pandademic@users.noreply.github.com>
2022-02-06 11:48:36 -0500

    Update prompt.go

--- prompt.go
+++ prompt.go
@@ -5,26 +5,37 @@ import (
      "github.com/spf13/viper"
 )
 func main() {
-		var os string = runtime.GOOS
-		viper.SetConfigName("dotshuttle") // name of config file (without extension)
-		viper.SetConfigType("yaml") // REQUIRED if the config file does not have the extension in the name
-		viper.AddConfigPath(".")   // path to look for the config file in
-		viper.AddConfigPath("$HOME/.appname")  // call multiple times to add many search paths
-		err := viper.ReadInConfig() // Find and read the config file
-		viper.SetDefault("promptSymbol", "$")
-		if err != nil { // Handle errors reading the config file
-				fmt.Errorf("NO CONFIG	: %w \n", err)
-		}	
-		switch os {
+	type config struct {
+		promptSymbol string
+		Git bool
+		multiLine bool
+		topLine string // only if multi line = true
+	}
+	var os string = runtime.GOOS
+	viper.SetConfigName("dotshuttle") // name of config file (without extension)
+	viper.SetConfigType("yaml") // REQUIRED if the config file does not have the extension in the name
+	viper.AddConfigPath(".")   // path to look for the config file in
+	viper.AddConfigPath("$HOME/.appname")  // call multiple times to add many search paths
+	err := viper.ReadInConfig() // Find and read the config file
+	viper.SetDefault("promptSymbol", "$")
+	viper.SetDefault("useGit",false)
+	viper.setDefault("multiLine",false)
+	viper.setDefault("topLine",false)// again only respected if multi-line = true
+	var C config
+	err := viper.Unmarshal(&C)
+	if err != nil{
+		fmt.Println("ERR: couldn't put push config into struct")
+	}
+	switch os {
 				
 		case "windows":
-				windowsPrompt()
+			windowsPrompt()
 				
 		case "darwin":
-				darwinPrompt()
+			darwinPrompt()
 		case "linux":
-				linuxPrompt()
-		}
+			linuxPrompt()
+	}
 
 }
 var (
@@ -39,7 +50,7 @@ func color(s string) func(...interface{}) string {
 	}
 }
 
-var osLogo string = "ﳑ"
+var osLogo string = ""
 func windowsPrompt(){
 		osLogo = ""
 		prompt()
@@ -53,6 +64,6 @@ func linuxPrompt() {
 		prompt()
 }
 func prompt() {
-	fmt.Println(osLogo + " "+viper.GetString("promptSymbol"))
+	fmt.Println(osLogo + " "+C.promptSymbol)
 
 }
