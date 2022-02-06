b9ab0c38accc5030b6240f9dd7a78ad825fac293
Atharv Gupte <74566464+Pandademic@users.noreply.github.com>
2022-02-06 11:52:13 -0500

    Update prompt.go

--- prompt.go
+++ prompt.go
@@ -21,7 +21,7 @@ func main() {
 	viper.SetDefault("useGit",false)
 	viper.SetDefault("multiLine",false)
 	viper.SetDefault("topLine",false)// again only respected if multi-line = true
-	var conf config 
+	var conf := config{promptSymbol:"",Git:false,multiLine:false,topLine:""} 
 	err := viper.Unmarshal(&C)
 	if err != nil{
 		fmt.Println("ERR: couldn't put push config into struct")
