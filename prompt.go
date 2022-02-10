c96ebd4b028ec6cc699be4a9f440f879596bbcc3
Atharv Gupte <74566464+Pandademic@users.noreply.github.com>
2022-02-10 16:40:23 -0500

    Update prompt.go

--- prompt.go
+++ prompt.go
@@ -65,7 +65,7 @@ func prompt(osLogo string) {
 	cwd := os.Getwd
 	viper.AutomaticEnv
 	homeVar := viper.Get("HOME")
-	if yesTruncDir{
+	if yesTruncDir == true{
 		prompt = prompt + red(trimPath(cwd,homeVar)
 	}
 	prompt = prompt + "" + cyan(icon)
